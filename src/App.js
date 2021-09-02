cb36e21a5b961958a1719fd63d5e92c70f8a9e3b
Harry Burgess <harryburgessjd@gmail.com>
2021-09-02 13:36:27 +0100

    bug: Stop windows opening if already opened

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   const [openedByFolder, setOpenedByFolder] = useState(false);
   const [currentActiveWindow, setCurrentActiveWindow] = useState("fileManager");
   const [fileWindows, setFileWindows] = useState([
-    { windowID: 1, fileName: "file-window-placeholder.png" },
-    { windowID: 2, fileName: "file-window-placeholder.png" },
+    { windowID: "fileWindow98", fileName: "file-window-placeholder.png" },
+    { windowID: "fileWindow99", fileName: "file-window-placeholder.png" },
   ]);
   const folderIconClickedHandler = () => {
     setOpenedByFolder(() => {
@@ -22,19 +22,27 @@ function App() {
   const activeWindowHandler = (id) => {
     setCurrentActiveWindow(id);
   };
-  const getRandomInt = (max) => {
-    return Math.floor(Math.random() * max);
-  };
   const openFileHandler = (fileInfo) => {
-    const randomNum = getRandomInt(99999);
-    setFileWindows(() => {
-      return [...fileWindows, { windowID: randomNum, ...fileInfo }];
-    });
-    setCurrentActiveWindow(`fileWindow${randomNum}`);
+    const idNumber = fileInfo.id;
+    const idExists = () => {
+      return fileWindows.some((file) => {
+        return file.windowID === `fileWindow${idNumber}`;
+      });
+    };
+    console.log(idExists());
+    if (!idExists()) {
+      setFileWindows(() => {
+        return [
+          ...fileWindows,
+          { windowID: `fileWindow${idNumber}`, ...fileInfo },
+        ];
+      });
+    }
+    setCurrentActiveWindow(`fileWindow${idNumber}`);
   };
   const removeFromArrayHandler = (removeId) => {
     const fileRemovedArray = fileWindows.filter(
-      (file) => `fileWindow${file.windowID}` !== removeId
+      (file) => file.windowID !== removeId
     );
     console.log(removeId);
     console.log(fileRemovedArray);
@@ -55,12 +63,8 @@ function App() {
         return (
           <FileWindow
             activeWindow={activeWindowHandler}
-            active={
-              currentActiveWindow === `fileWindow${window.windowID}`
-                ? true
-                : false
-            }
-            id={`fileWindow${window.windowID}`}
+            active={currentActiveWindow === window.windowID ? true : false}
+            id={window.windowID}
             key={window.windowID}
             image={window.fileName}
             text={window.about}
